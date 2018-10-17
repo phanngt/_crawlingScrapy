5ba1dbce4788fffe4fa7f093096d759cc71c330f
Phan Nguyen <phanngt@gmail.com>
2018-10-17 17:27:27 +0700

    Remove stuffs

--- link_finder.py
+++ link_finder.py
@@ -22,21 +22,3 @@ class LinkFinder(HTMLParser):
 
     def page_links(self):
         return self.links
-
-
-finder = LinkFinder('https://mp3.zing.vn/', 'https://mp3.zing.vn/')
-finder.feed('<li><a id="navbar-top100" title="Top 100" href="/top100/Nhac-Tre/IWZ9Z088.html">Top 100</a>'
-                '<div class="megamenu submenu menu-col-1">'
-                    '<div class="subcol menu-col-1-narrow">'
-                        '<div class="subinner_item">'
-                            '<ul>'
-                                '<li><a title="Việt Nam" href="/top100/Nhac-Tre/IWZ9Z088.html">Việt Nam</a></li>'
-                                '<li><a title="Âu Mỹ" href="/top100/Pop/IWZ9Z097.html">Âu Mỹ</a></li>'
-                                '<li><a title="Châu Á" href="/top100/Kpop/IWZ9Z08W.html">Châu Á</a></li>'
-                                '<li><a title="Hòa Tấu" href="/top100/Classical/IWZ9Z0BI.html">Hòa Tấu</a></li>'
-                            '</ul>'
-                        '</div>'
-                    '</div><!--End .sub-col -->'
-                '</div>'
-            '</li>')
-
